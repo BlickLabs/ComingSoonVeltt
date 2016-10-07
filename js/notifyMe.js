4e802cdb7776ae4f85347d17e76f923c00b33d2e
Alan Sanchez <sanchezpineda03@gmail.com>
2016-10-06 20:54:20 -0500

    Modified error messages text.

--- js/notifyMe.js
+++ js/notifyMe.js
@@ -9,11 +9,11 @@
     // Default options.
   	var settings = $.extend({
 	  	// Error and success message strings
-	    msgError404: "Service is not available at the moment. Please check your internet connection or try again later.",
-			msgError503: "Oops. Looks like something went wrong. Please try again later.",
-			msgErrorValidation: "This email address looks fake or invalid. Please enter a real email address.",
-			msgErrorFormat: "Your e-mail address is incorrect.",
-			msgSuccess: "Congrats! You are in list."
+	    msgError404: "Servicio no disponible por el momento.",
+			msgError503: "Algo salió mal. Intentalo de nuevo más tarde.",
+			msgErrorValidation: "La dirección de correo electrónico es inválida o no existe. Porfavor escribe una dirección válida.",
+			msgErrorFormat: "La dirección de correo electrónico es incorrecta.",
+			msgSuccess: "Mensaje enviado exitosamente."
 		}, options );
 
   
