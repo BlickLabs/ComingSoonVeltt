c05ad9c9dfde1626e9a245be3e89b18d8cd82569
Mauricio Ivan Mejia Ramirez <mauriciodinki@gmail.com>
2016-10-09 18:42:19 -0500

    Add already register validation

--- js/notifyMe.js
+++ js/notifyMe.js
@@ -13,26 +13,27 @@
 			msgError503: "Algo salió mal. Intentalo de nuevo más tarde.",
 			msgErrorValidation: "La dirección de correo electrónico es inválida o no existe.",
 			msgErrorFormat: "La dirección de correo electrónico es incorrecta.",
-			msgSuccess: "Suscripción realizada con éxito."
+			msgSuccess: "Suscripción realizada con éxito.",
+			msgAlready: "Actualmente ya te encuentras registrado"
 		}, options );
 
-  
+
 		var form = $(this);
 		var input = form.find("input[name=email]");
 		var button = form.find("button");
 		var loader = form.closest(".notify-wrap").children(".loader-container");
-		
+
 		var action = form.attr("action");
 		var message = $("<div class='col-lg-12 align-center' id='message'></div>").appendTo(form);
 
 		loader.hide();
-	  
+
 		form.on("submit", function (e) {
 			e.preventDefault();
 			// Test if the value of input is actually an email
 			$.ajax({
 	      type: 'POST',
-	      url: action, 
+	      url: action,
 	      data: form.serialize(),
 	      beforeSend: function () {
 	        var isEmail = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
@@ -48,7 +49,9 @@
 	      success: function (data) {
 	        if (data.status == 'subscribed') {
 	          message.text(settings.msgSuccess);
-	        } else if (data.type == "ValidationError") { 
+					} else if (data.title === 'Member Exists') {
+						message.text(settings.msgAlready);
+	        } else if (data.type == "ValidationError") {
 						message.text(settings.msgErrorValidation);
 					} else {
 						message.text(settings.msgError503);
